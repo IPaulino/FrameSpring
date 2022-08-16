38f4aab5f0c15e8c265bb79b91c84a568aa1430d
IversonPaulino <iversonoliveira1999@live.com>
2022-08-15 21:48:49 -0300

    Ajustes, Adição de comentários.

--- src/main/java/com/uniesp/Aulas/ControllerApplication.java
+++ src/main/java/com/uniesp/Aulas/ControllerApplication.java
@@ -2,12 +2,10 @@ package com.uniesp.Aulas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class ControllerApplication {
 
-	@RequestMapping
 	public static void main(String[] args) {
 		SpringApplication.run(ControllerApplication.class, args);
 	}
